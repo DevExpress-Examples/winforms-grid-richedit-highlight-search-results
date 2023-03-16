711cc9b29acb77c4bdffc2167ead8c8e8807fcca
DevExpressExampleBot <fakeEmail>
2023-03-16 18:28:02 +0400

    Source auto update [skip ci]

--- VB/E4422/Program.vb
+++ VB/E4422/Program.vb
@@ -1,21 +1,20 @@
-﻿Imports Microsoft.VisualBasic
+Imports DevExpress.XtraRichEdit
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Forms
 
 Namespace E4422
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            RichEditControlCompatibility.UseThemeFonts = False
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
